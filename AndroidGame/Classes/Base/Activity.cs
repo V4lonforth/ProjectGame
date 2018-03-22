fefa53fbd281e20f5eb7648b2fb1233ac789a454
V4lonforth <v4lonforth@gmail.com>
2018-03-22 18:53:31 +0300

    Game

--- AndroidGame/Classes/Base/Activity.cs
+++ AndroidGame/Classes/Base/Activity.cs
@@ -2,6 +2,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Views;
+using AndroidGame.Controllers;
 
 namespace AndroidGame
 {
@@ -11,7 +12,7 @@ namespace AndroidGame
         , Theme = "@style/Theme.Splash"
         , AlwaysRetainTaskState = true
         , LaunchMode = LaunchMode.SingleInstance
-        , ScreenOrientation = ScreenOrientation.FullUser
+        , ScreenOrientation = ScreenOrientation.UserLandscape
         , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize)]
 
     public class Activity : Microsoft.Xna.Framework.AndroidGameActivity
@@ -19,6 +20,10 @@ namespace AndroidGame
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            var metrics = Resources.DisplayMetrics;
+            GUIController.screenSize.X = metrics.WidthPixels;
+            GUIController.screenSize.Y = metrics.HeightPixels;
+
             var g = new AndroidGame();
             SetContentView((View)g.Services.GetService(typeof(View)));
             g.Run();
---
+++ AndroidGame/Classes/Controllers/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using AndroidGame.Serialization;
+using AndroidGame.GameObjects.Ships;
+
+namespace AndroidGame.Controllers
+{
+    class GameController : IController
+    {
+        private IController[] controllers;
+        
+        public GameController(ContentManager Content)
+        {
+            SerializationManager serializationManager = new SerializationManager();
+
+            ProjectilesController projectilesController = new ProjectilesController(Content, serializationManager);
+            LootController lootController = new LootController(Content);
+            ShipsController shipsController = new ShipsController(Content, serializationManager, lootController, projectilesController);
+            PlayerShip playerShip = shipsController.CreatePlayerShip(Vector2.One * 250, Vector2.UnitX);
+            GUIController GUIController = new GUIController(Content, playerShip.SetMovementDirection, playerShip.SetAttackDirection);
+
+            controllers = new IController[]
+            {
+                projectilesController,
+                lootController,
+                shipsController,
+                GUIController
+            };
+        }
+
+        public void Update(float deltaTime)
+        {
+            foreach (IController controller in controllers)
+                controller.Update(deltaTime);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (IController controller in controllers)
+                controller.Draw(spriteBatch);
+        }
+    }
+}
