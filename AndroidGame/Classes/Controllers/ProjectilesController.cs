277e392ce6284cc1396f4c4f05e43d399cbb753c
V4lonforth <v4lonforth@gmail.com>
2018-03-22 18:35:56 +0300

    Projectiles

---
+++ AndroidGame/Classes/Controllers/ProjectilesController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using AndroidGame.GameObjects;
+using AndroidGame.Serialization;
+
+namespace AndroidGame.Controllers
+{
+    class ProjectilesController : IController
+    {
+        private List<List<Projectile>> activeProjectiles;
+        private List<List<Projectile>> inactiveProjectiles;
+
+        private Texture2D[] projectilesSprites;
+        private ProjectileInfo[] projectilesInfo;
+
+        private const string projectileSpritesPath = "Sprites/Projectiles/Projectile";
+
+        private const int projectilesStartCount = 30;
+
+        public ProjectilesController(ContentManager Content, SerializationManager serializationManager)
+        {
+            activeProjectiles = new List<List<Projectile>>();
+            inactiveProjectiles = new List<List<Projectile>>();
+            projectilesInfo = serializationManager.LoadInfo<ProjectileInfo>("Projectiles");
+            projectilesSprites = new Texture2D[projectilesInfo.Length];
+
+            for (int i = 0; i < projectilesSprites.Length; i++)
+                projectilesSprites[i] = Content.Load<Texture2D>(projectileSpritesPath + i.ToString());
+            for (int i = 0; i < projectilesInfo.Length; i++)
+                CreateProjectiles(projectilesInfo[i], projectilesStartCount);
+        }
+
+        private void CreateProjectiles(ProjectileInfo info, int count)
+        {
+            while (inactiveProjectiles.Count <= info.projectileType)
+            {
+                inactiveProjectiles.Add(new List<Projectile>());
+                activeProjectiles.Add(new List<Projectile>());
+            }
+            for (int i = 0; i < count; i++)
+                inactiveProjectiles[info.projectileType].Add(new Projectile(info, projectilesSprites));
+        }
+        private void RemoveProjectile(int projectileType, int index)
+        {
+            Projectile projectile = activeProjectiles[projectileType][index];
+            activeProjectiles[projectileType].RemoveAt(index);
+            inactiveProjectiles[projectileType].Add(projectile);
+            if (inactiveProjectiles[projectileType].Count > projectilesStartCount)
+                inactiveProjectiles[projectileType].RemoveAt(inactiveProjectiles.Count - 1);
+        }
+
+        public void LaunchProjectile(Vector2 pos, Vector2 dir, float damage, int team, int projectileType)
+        {
+            if (inactiveProjectiles.Count <= projectileType || inactiveProjectiles[projectileType].Count == 0)
+                CreateProjectiles(projectilesInfo[projectileType], 5);
+            Projectile projectile = inactiveProjectiles[projectileType][inactiveProjectiles.Count - 1];
+            projectile.Launch(pos, dir, damage, team);
+            activeProjectiles[projectileType].Add(projectile);
+            inactiveProjectiles[projectileType].RemoveAt(inactiveProjectiles.Count - 1);
+        }
+
+        public void Update(float deltaTime)
+        {
+            for (int i = 0; i < activeProjectiles.Count; i++)
+            {
+                for (int j = 0; j < activeProjectiles[i].Count; j++)
+                {
+                    if (activeProjectiles[i][j].IsDestroyed)
+                    {
+                        RemoveProjectile(i, j);
+                        j--;
+                    }
+                    else
+                        activeProjectiles[i][j].Update(deltaTime);
+                }
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (List<Projectile> projectiles in activeProjectiles)
+                foreach (Projectile projectile in projectiles)
+                    projectile.Draw(spriteBatch);
+        }
+    }
+}
---
+++ AndroidGame/Classes/GameObjects/Projectile.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using AndroidGame.Physics;
+using AndroidGame.Serialization;
+using AndroidGame.GameObjects.Base;
+using AndroidGame.GameObjects.Ships;
+
+namespace AndroidGame.GameObjects
+{
+    class Projectile : PhysicalObject, IPhysicalObject
+    {
+        public bool IsDestroyed { get; private set; }
+
+        public float Damage { get; private set; }
+        public int Team { get; private set; }
+
+        private float timeToDestroy;
+        private const float liveTime = 5f;
+
+        public Projectile(ProjectileInfo info, Texture2D[] sprites) 
+            : base(new Drawable(sprites[info.spriteIndex], size: info.spriteSize), info.speed, PhysicalObjectType.Projectile, Vector2.Zero, Vector2.Zero, Vector2.Zero, info.bodyInfo)
+        {
+        }
+
+        public void Launch(Vector2 pos, Vector2 dir, float damage, int team)
+        {
+            Team = team;
+            IsDestroyed = false;
+            Position = pos;
+            LookingDirection = MovementDirection = dir;
+            Damage = damage;
+            timeToDestroy = liveTime;
+        }
+
+        public new void Update(float deltaTime)
+        {
+            base.Update(deltaTime);
+            timeToDestroy -= deltaTime;
+            if (timeToDestroy <= 0f)
+                IsDestroyed = true;
+        }
+
+        protected override bool OnCollision(Body body)
+        {
+            switch(body.Parent.Type)
+            {
+                case PhysicalObjectType.Ship:
+                    if (((Ship)body.Parent).Team == Team)
+                        return false;
+                    IsDestroyed = true;
+                    return true;
+            }
+            return false;
+        }
+    }
+}
