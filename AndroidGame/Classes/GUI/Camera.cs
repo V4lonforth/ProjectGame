8af2722daadaa4245f0f48fda237492ca36e580d
red <v4lonforth@gmail.com>
2018-04-30 16:07:45 +0300

    GUI

--- AndroidGame/Classes/GUI/Camera.cs
+++ AndroidGame/Classes/GUI/Camera.cs
@@ -4,31 +4,41 @@ using AndroidGame.GameObjects.Base;
 
 namespace AndroidGame.GUI
 {
-    class Camera
+    public class Camera
     {
+        public PhysicalObject Target
+        {
+            private get { return target; }
+            set
+            {
+                target = value;
+                cameraPosition = target.Position;
+                Update(0f);
+            }
+        }
+
         private PhysicalObject target;
-        
+
         private Vector2 viewportCentre;
         private Vector2 cameraPosition;
 
         private float size;
-
+        
         public Matrix TransformMatrix { get; private set; }
 
         private const float lerpValue = 3f;
 
-        public Camera(Point screenSize, PhysicalObject targ)
+        public Camera(Point screenSize)
         {
-            size = 0.5f;
+            size = 0.35f;
             viewportCentre = screenSize.ToVector2() / (2f * size);
-            cameraPosition = targ.Position;
-            target = targ;
         }
 
-        public void MoveCamera(float delta)
+        public void Update(float deltaTime)
         {
-            cameraPosition = Functions.Lerp(cameraPosition, target.Position, lerpValue * delta);
+            cameraPosition = Functions.Interpolate(cameraPosition, Target.Position, lerpValue * deltaTime);
             TransformMatrix = Matrix.CreateTranslation(viewportCentre.X - cameraPosition.X, viewportCentre.Y - cameraPosition.Y, 0) * Matrix.CreateScale(size);
+            Vector3 vec = Vector3.Transform(new Vector3(360, 640, 0), TransformMatrix);
         }
     }
 }
